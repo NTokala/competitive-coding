88f4657627e57f5d2d767c3c59f6bf0fe9fcd647
Nikhil Tokala <35087932+NTokala@users.noreply.github.com>
2018-01-18 15:36:28 -0500

    Create 1watermelon.cpp

---
+++ Codeforces/Problemset/1watermelon.cpp
@@ -0,0 +1,29 @@
+/*
+	Author: ntokala/sleepyd3v
+*/
+#include <iostream>
+#define ld long double;
+#define FAST ios_base::sync_with_stdio(0); cin.tie(0);
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define FOR(i,a,b) for (int i = a; i < b; i++) //<, not <=!
+#define F0R(i, b) for (int i = 0; i < b; i++)
+typedef long long ll;
+
+using namespace std;
+
+int main(){
+	FAST;
+	int w; cin >> w;
+	if(w > 2){
+		if(w % 2 == 0)
+			cout << "YES" << "\n";
+		else
+			cout << "NO" << "\n";
+	}
+	else
+		cout << "NO" << "\n";
+	return 0;
+}
